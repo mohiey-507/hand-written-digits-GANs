ad45959c94048392fa45d3ee5488171e6003edaa
mohiey <mohamedmohiey507@gmail.com>
2025-04-03 00:01:52 +0200

    Hide axes when visualizing images

--- GANS/DC-GAN/utils.py
+++ GANS/DC-GAN/utils.py
@@ -10,5 +10,6 @@ def show_tensor_images(image_tensor, num_images=25, size=(1, 28, 28)):
     image_tensor = (image_tensor + 1) / 2
     image_unflat = image_tensor.detach().cpu()
     image_grid = make_grid(image_unflat[:num_images], nrow=5)
-    plt.imshow(image_grid.permute(1, 2, 0).squeeze())
+    plt.imshow(image_grid.permute(1, 2, 0).squeeze(), cmap='gray')
+    plt.axis('off')
     plt.show()
