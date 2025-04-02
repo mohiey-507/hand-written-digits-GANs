21caad91e2eb21fea86e82580a76fdf8c710d247
mohiey <mohamedmohiey507@gmail.com>
2025-04-02 10:38:26 +0200

    implemented Discriminator loss

--- GANS/DC-GAN/loss.py
+++ GANS/DC-GAN/loss.py
@@ -10,3 +10,18 @@ class GenLoss(nn.Module):
         fake_targets = torch.ones_like(fake_logits)
         gen_loss = self.criterion(fake_logits, fake_targets)
         return gen_loss.item()
+
+class DiscLoss(nn.Module):
+    def __init__(self):
+        super(DiscLoss, self).__init__()
+        self.criterion = nn.BCEWithLogitsLoss()
+
+    def forward(self, real_logits, fake_logits):
+        fake_targets = torch.zeros_like(fake_logits)
+        real_targets = torch.ones_like(real_logits)
+
+        fake_loss = self.criterion(fake_logits, fake_targets)
+        real_loss = self.criterion(real_logits, real_targets)
+
+        disc_loss = (real_loss + fake_loss) / 2
+        return disc_loss.item()
