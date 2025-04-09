c0782bde1264ad2e1875cee59dff68116db3ba83
mohiey <mohamedmohiey507@gmail.com>
2025-04-09 18:22:43 +0200

    Updata Discriminator architecture

--- GANS/Conditional-GAN/models/disc.py
+++ GANS/Conditional-GAN/models/disc.py
@@ -2,34 +2,32 @@ import torch
 import torch.nn as nn
 
 class Discriminator(nn.Module):
-    def __init__(self, input_dim, hidden_dim=16):
+    def __init__(self, input_dim: int = 11, hidden_dim: int = 32):
         super(Discriminator, self).__init__()
 
-        # Adjust input channels of the first layer
-        self.block1 = nn.Sequential(
-            # input_dim = im_chan + n_classes
-            nn.Conv2d(input_dim, hidden_dim, kernel_size=4, stride=2, padding=1, bias=False),
-            nn.BatchNorm2d(hidden_dim),
-            nn.LeakyReLU(0.2)
-        )
-        # Output shape: (hidden_dim, 14, 14)
+        self.layers = nn.Sequential(
+        # Input: (input_dim, 1, 1) -> Output: (hidden_dim, 14, 14)
+            nn.Conv2d(in_channels=input_dim, out_channels=hidden_dim,
+                    kernel_size=4, stride=2, padding=1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
 
-        self.block2 = nn.Sequential(
-            nn.Conv2d(hidden_dim, hidden_dim * 2, kernel_size=4, stride=2, padding=1, bias=False), # Adjusted padding for size
+        # Input: (hidden_dim, 14, 14) -> Output: (hidden_dim * 2, 7, 7)
+            nn.Conv2d(in_channels=hidden_dim, out_channels=hidden_dim * 2,
+                    kernel_size=4, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(hidden_dim * 2),
-            nn.LeakyReLU(0.2)
-        )
-        # Output shape: (hidden_dim*2, 7, 7)
+            nn.LeakyReLU(0.2, inplace=True),
 
-        self.block3 = nn.Sequential(
-            nn.Conv2d(hidden_dim * 2, 1, kernel_size=7, stride=1, padding=0, bias=False) # Adjusted kernel_size
+        # Input: (hidden_dim * 2, 7, 7) -> Output: (hidden_dim * 4, 3, 3)
+            nn.Conv2d(in_channels=hidden_dim * 2, out_channels=hidden_dim * 4,
+                    kernel_size=3, stride=2, padding=0, bias=False),
+            nn.BatchNorm2d(hidden_dim * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+
+        # Input: (hidden_dim * 4, 3, 3) -> Output: (1, 1, 1)
+            nn.Conv2d(in_channels=hidden_dim * 4, out_channels=1,
+                    kernel_size=3, stride=1, padding=0, bias=False)
         )
-        # Output shape: (1, 1, 1)
 
     def forward(self, image_and_labels):
-        # image_and_labels shape is : (batch_size, input_dim, 28, 28)
-        x = self.block1(image_and_labels)
-        x = self.block2(x)
-        x = self.block3(x)
-        # Reshape to (batch_size, 1)
+        x = self.layers(image_and_labels)
         return x.view(x.size(0), -1)
